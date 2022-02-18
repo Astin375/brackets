3180dc17ba0ce86dc761a0e4bce4c44315d70d7c
Artsemi <stojanka25@gmail.com>
2022-02-18 23:28:18 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  if(str.split('').length % 2 !== 0){
+  let arr = str.split('');
+  // работает и без этой проверки, но почему бы не избежать лишней работы
+  if(arr.length % 2 !== 0){
     return false;
   } else{
-    return true;
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(arr[i] === bracketsConfig[j][0] && arr[i+1] === bracketsConfig[j][1]){
+        arr.splice(i, 2);
+        i-=2;
+      }
+    }
   }
+  if(arr.length === 0){
+    return true
+  } else{
+    return false
+  }
+}
 }
